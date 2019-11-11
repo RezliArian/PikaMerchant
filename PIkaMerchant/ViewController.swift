d43c63ce250b1b5a4c1976214c882b3c7a0fd543
Rezli <rezlidpresley@gmail.com>
2019-11-11 17:31:29 +0700

    Initial Commit

---
+++ PIkaMerchant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PIkaMerchant
+//
+//  Created by Rezli Arian Perdana on 11/11/19.
+//  Copyright © 2019 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
