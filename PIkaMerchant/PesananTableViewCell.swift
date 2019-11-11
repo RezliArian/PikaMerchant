67e31064d718b3caf7185a4b2f06d987d4a9bd70
Rezli <rezlidpresley@gmail.com>
2019-11-11 23:26:02 +0700

    bikin file swift dan xib buat cell pesanan

---
+++ PIkaMerchant/PesananTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PesananTableViewCell.swift
+//  PIkaMerchant
+//
+//  Created by Rezli Arian Perdana on 11/11/19.
+//  Copyright © 2019 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class PesananTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
