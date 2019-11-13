7ae45d3640a4720d67772ea01b74a44c0cadd485
Rezli <rezlidpresley@gmail.com>
2019-11-13 10:42:29 +0700

    create xib for food table

---
+++ PIkaMerchant/Makanan/MakananTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MakananTableViewCell.swift
+//  PIkaMerchant
+//
+//  Created by Rezli Arian Perdana on 13/11/19.
+//  Copyright © 2019 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class MakananTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
